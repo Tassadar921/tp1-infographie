7eead5a3ef3bad13d64d1c8717fb2372138dc984
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-09-21 14:33:13 +0200

    exo2

--- exo2/ligne.js
+++ exo2/ligne.js
@@ -4,17 +4,37 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.5, 1000);
 const scene = new THREE.Scene();
+
+camera.position.set(0,0,100);
+
 scene.background = new THREE.Color(0, 0, 0);
 
-const material = new THREE.LineBasicMaterial({color: 0x1495A4})
+const purple = new THREE.LineBasicMaterial({color: 0x85138E});
+const green = new THREE.LineBasicMaterial({color: 0x3CAF0D});
 
-const points = [];
+const squareBuffer = [];
 
-points.push(new THREE.Vector3(2,1,0));
-points.push(new THREE.Vector3(4,-1,0));
+squareBuffer.push( new THREE.Vector3( 20, 30, 0 ) );
+squareBuffer.push( new THREE.Vector3( 50, 0, 0 ) );
+squareBuffer.push( new THREE.Vector3( 20, -30, 0 ) );
+squareBuffer.push( new THREE.Vector3( -10, 0, 0 ) );
+squareBuffer.push( new THREE.Vector3( 20, 30, 0 ) );
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const figure1 = new THREE.BufferGeometry().setFromPoints(squareBuffer);
 
-const line = new THREE.Line(geometry, material);
-scene.add(line);
+const square = new THREE.Line(figure1, purple);
+scene.add(square);
+
+const triangleBuffer = [];
+
+triangleBuffer.push( new THREE.Vector3( -40, 30, 0 ) );
+triangleBuffer.push( new THREE.Vector3( -10, 30, 0 ) );
+triangleBuffer.push( new THREE.Vector3( -40, 0, 0 ) );
+triangleBuffer.push( new THREE.Vector3( -40, 30, 0 ) );
+
+const figure2 = new THREE.BufferGeometry().setFromPoints(triangleBuffer);
+
+const triangle = new THREE.Line(figure2, green);
+scene.add(triangle);
+
 renderer.render(scene, camera);
